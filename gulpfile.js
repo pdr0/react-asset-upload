b526f3ab88648934041318be4c4e1663319d4ab5
pdr0 <yo@pedroselas.com>
2018-04-17 15:38:01 +0200

    Config gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,4 @@
 var gulp = require('gulp');
 
-var config = {
-    scripts: {
-        'assets/js/app.min.js': 'assets/js/main.js',
-        'assets/js/tracking.min.js': 'assets/js/tracking.js'
-    },
 
 };
